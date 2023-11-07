81639d7ebe88136cdc8580c5649ce4308a8208d2
tincho70 <mmiraglia@gmail.com>
2023-11-07 13:44:36 -0300

    Script for docker compose, tag and push updated

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -7,3 +7,5 @@ cd "$(readlink -e -- "${0%/*}/.." || true)"
 export TAG=$(jq -r '.version' package.json)
 
 docker compose -f docker-compose.prod.yml build
+docker tag tincho70/bot-pozo:$TAG tincho70/bot-pozo:latest
+docker push tincho70/bot-pozo -a
